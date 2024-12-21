47b5d13e164a6752389313bd65893bfc420a2fea
spineda2019 <spineda.wpi.alum@gmail.com>
2024-12-21 15:01:57 -0500

    skeleton arg parsing

--- day_one/src/main.zig
+++ day_one/src/main.zig
@@ -1,24 +1,23 @@
 const std = @import("std");
 
+const Error = error{
+    BadArgCount,
+};
+
+// !void is an anyerror return type
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    var alloc = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = alloc.allocator();
+    defer _ = alloc.deinit();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    const iterator = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, iterator);
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    if (iterator.len != 2) {
+        std.debug.print("Invalid amount of arguments: {d}\n", .{iterator.len});
+        return Error.BadArgCount;
+    } else {
+        const filepath = iterator[1];
+        std.debug.print("Puzzle input given: {s}\n", .{filepath});
+    }
 }
