253ea2d56565e2d55f0ec1e7ba1b183d6749da18
spineda2019 <spineda.wpi.alum@gmail.com>
2024-12-23 19:49:03 -0500

    bump

--- day_four/src/xmas_finder.zig
+++ day_four/src/xmas_finder.zig
@@ -7,6 +7,10 @@ pub fn find(filename: []const u8) !void {
 
     var line_buffer: [4096]u8 = .{0} ** 4096;
 
+    var file_buffer: [140][140]u8 = .{.{0} ** 140} ** 140;
+
+    _ = &file_buffer;
+
     while (try in_reader.readUntilDelimiterOrEof(&line_buffer, '\n')) |line| {
         _ = line;
     }
